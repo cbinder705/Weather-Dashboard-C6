86fe78673f4429290521e9fc5b2d1e2f198f2ab6
Cory Bee <cbinder705@gmail.com>
2022-10-24 18:16:59 -0400

    added function for displaying with and object for weather data

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 let weather = {
   apiKey: "106f2f72e03bbf98c782d152c4ecb390",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=standard&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+  },
 };
