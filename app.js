4b48eda36f044f4ee814581e8572fa5fe5936269
Cory Bee <cbinder705@gmail.com>
2022-12-30 16:22:35 -0500

    updated app.js

--- app.js
+++ app.js
@@ -12,10 +12,13 @@ let weather = {
   },
   displayWeather: function (data) {
     const { name } = data;
+    const { icon, description } = data.weather;
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, temp, humidity, speed);
+    console.log(name, icon, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".temp").innerText = temp + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
