5746bf1fe780f5aa8c71c47a1d350036560fe22e
Cory Bee <cbinder705@gmail.com>
2022-12-30 16:43:07 -0500

    updated app.js

--- app.js
+++ app.js
@@ -12,17 +12,33 @@ let weather = {
   },
   displayWeather: function (data) {
     const { name } = data;
-    const { icon, description } = data.weather;
+    const { description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
+    console.log(name, description, temp, humidity, speed);
     document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
     document.querySelector(".temp").innerText = temp + "°F";
     document.querySelector(".humidity").innerText =
       "Humidity: " + humidity + "%";
     document.querySelector(".wind-speed").innerText =
       "Wind speed: " + speed + " km/h";
   },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Philadelphia");
